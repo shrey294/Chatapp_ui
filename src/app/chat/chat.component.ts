c4d40e053369353911e89513f9e0fc2dc1fdb01e
shrey294 <gshrey470@gmail.com>
2025-05-08 10:51:08 +0530

    added notification

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -16,11 +16,16 @@ export class ChatComponent implements OnInit, AfterViewChecked {
  @ViewChild('scrollMe') private ScrollContainer!: ElementRef
  Router = inject(Router)
  roomname = sessionStorage.getItem("room");
+ searchText:string='';
 
  ngOnInit(): void {
+  if ('Notification' in window && Notification.permission !== 'granted') {
+    Notification.requestPermission();
+  }
+
   this.chatService.messages$.subscribe(res=>{
     this.messages = res;
-    console.log(this.messages);
+    this.handleIncomingMessage(res[res.length - 1]);
     
   });
 }
@@ -44,5 +49,27 @@ leavechat(){
   }).catch((err)=>{
     console.log(err);
   })
+}
+get filteredUsers(){
+  const users = this.chatService.ConnectedUsers$.value;
+  if(!this.searchText) return users;
+  return users.filter(user=>user.toLowerCase().includes(this.searchText.toLowerCase()));
+}
+isTabInactive(): boolean {
+  return document.hidden;
+}
+handleIncomingMessage(message: any) {
+  if (message.user !== this.loggedInUserName && this.isTabInactive()) {
+    this.showNotification(`New message from ${message.user}`, message.message);
+  }
+}
+showNotification(title: string, body: string) {
+  if (Notification.permission === 'granted') {
+    const notification = new Notification(title, { body });
+    notification.onclick = () => {
+      window.focus();
+      notification.close();
+    };
+  }
 }
 }
